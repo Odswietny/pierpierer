7925c8b468f88cc732daa2b101baad5b06ac2a50
Odświętny <odswietny@gmail.com>
2025-10-21 19:31:15 +0200

    Add files via upload

---
+++ pierpierer.py
@@ -0,0 +1,19 @@
+import pyperclip
+import time
+
+print("PierPierer v1.0.0")
+try:
+    howmany = int(input("Ile chcesz powtorzen czesci 'pier'?: "))
+except ValueError:
+    print("ERROR: nie mozesz wstawic nic innego niz liczbe! Zamkniecie za 5 sekund...")
+    time.sleep(5)
+    exit(1)
+before = input("Czy chcesz cos wstawic na poczatku? (nie wpisuj nic jesli nie): ")
+after = input("Czy chcesz cos wstawic na koncu? (nie wpisuj nic jesli nie): ")
+
+newstring = before + "pier" * abs(howmany) + after
+print(newstring)
+pyperclip.copy(newstring)
+print("Tekst zostal skopiowany do schowka. Zamkniecie za 5 sekund...")
+time.sleep(5)
+exit(0)
